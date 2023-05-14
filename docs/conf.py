25f87eb77940a7eb2206f98f2842aba08d0e4c7f
maciej.filanowicz <mfilon13@gmail.com>
2023-05-14 13:21:25 +0200

    auto docstring

--- docs/conf.py
+++ docs/conf.py
@@ -23,12 +23,12 @@ extensions = [
     "sphinx.ext.doctest",
     "sphinx.ext.autodoc",
 ]
-templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store","setup.py"]
+templates_path = ["../_templates"]
+exclude_patterns = ["../_build", "../Thumbs.db", "../.DS_Store","../setup.py"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+html_static_path = ["../_static"]
